ae62d8a7d86c24bfc1e65dc7d1bee3b11c073fd2
Dickson Valkova <kurumi000@proton.me>
2025-10-16 21:26:21 -0500

    se encriptaron las contrasenas

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 //Importacion de paquetes necesarios
-const {User, Game, Review} = require('./models/models.js');
-const connectDB = require('./config/db.js');
-const mongoose = require('mongoose');
-const express = require('express');
-const chalk = require('chalk');
-const path = require('path');
+const {User, Game, Review} = require('./models/models.js'); //Modelo de la db
+const connectDB = require('./config/db.js'); //conectar a la base de datos
+const mongoose = require('mongoose');//requerimiento para la base d datoas
+const express = require('express');//express para el server
+const bcrypt = require('bcrypt');//para el encriptado
+const chalk = require('chalk');//para los colores de la consola
+const path = require('path');//para las rutas y archivos
+const xss = require('xss');
+
+//Nivel de encryptado
+const hashLevel = 10
 
 const app = express();
 const port = 8080;
@@ -13,6 +18,7 @@ const port = 8080;
 connectDB();
 
 app.use(express.json());
+app.use(xss());
 app.use(express.urlencoded({extended: true}));
 //Archivos de public (para que se vea la pagina)
 app.use(express.static(path.join(__dirname, 'public')));
@@ -26,11 +32,56 @@ app.get('/', (req, res) => {
 app.get("/register", (req, res) => {
 	res.sendFile(path.join(__dirname, 'public', 'register.html'));		
 }); 
+
+//Acceso a la pagina de crear cuenta
+app.get("/login", (req, res) => {
+	res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
+//Ingresar
+app.post("/login", async (req, res) => {
+	try{
+		let {identifier, password} = xss(req.body);
+		
+		//Verificar si llego algo del js
+		if(!identifier || !password){
+			return res.json({success: false, messge: "Faltan campos"});
+		}
+
+		const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+		let query = {};
+		if(emailRegex.test(identifier)){
+			query = {email: identifier}
+		}else{
+			query = {username: identifier};
+		}
+
+		//Buscar wl usuarioo
+		let user = await User.findOne(query);
+		if(!user){
+			return res.json({success: false, message: "Usuario no encontrado"});
+		}
+
+		//Validar lacontrasenia
+		const validPassword = await bcrypt.compare(password, user.password);
+
+		if(validPassword){
+			return res.json({success: false, message: "Contrasena incorrecta"});
+		}
+
+		res.send("Inicio de sesion completado")
+			
+
+	}catch(error){
+		console.log(error);
+	}
+});
+
 //Crear usuario
 app.post("/register", async (req, res) => {
 	try{
 		//Verificar si el usuario y email existen
-		const {username, email} = req.body;
+		let {username, email, password} = req.body;
 
 		usernameExist = await User.findOne({username});
 		emailExist = await User.findOne({email});
@@ -40,7 +91,11 @@ app.post("/register", async (req, res) => {
 		}if(emailExist){
 			return res.send(`El correo ${email} ya esta registrado, elige otro correo.`);
 		}else{
-			const newUser = new User(req.body);
+			//Encryptar contrasenia
+			let hashedPassword = await bcrypt.hash(password, hashLevel);
+			
+			//Crear usuario
+			const newUser = new User({username, email, password: hashedPassword});
 			await newUser.save();
 			res.send(`Se creo el usuario ${newUser.username}.`);
 		}
